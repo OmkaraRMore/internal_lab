2f93d0cafa59e330f5aa8125b62bc5e5a32bffe9
omkar more <omkarrmore123@gmail.com>
2022-07-09 20:10:45 +0530

    done

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,11 @@
 #prime numbers
-num= 5
+num= 6
 for i in range(2,num):
     if (num % i ==0):
         print("The number is not prime")
         break
 else:
     print("The number is prime number")
+print("Changes are done")
     
   
